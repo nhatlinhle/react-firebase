8808d2e40ca002871053d5636a526a8737c5fbfd
Truong NTA <truongpnn.nta@gmail.com>
2024-10-14 16:46:04 +0700

    [IMPLEMENT]: build layout chat responsive mobile 14_10_2024 company

--- src/pages/chat/index.tsx
+++ src/pages/chat/index.tsx
@@ -1,16 +1,27 @@
 import "./style.css";
 import Sidebar from "./components/sidebar";
 import Content from "./components/content";
-import MediaFile from "./components/mediaFile"
+import MediaFile from "./components/mediaFile";
+import { useState } from "react";
 
 export default function Chat() {
+  const [showSidebar, setShowSidebar] = useState(false);
+
+  const toggleSidebar = (visible: boolean) => {
+    setShowSidebar(visible);
+  };
+
   return (
     <>
       <div className="flex flex-row p-0 sm:p-[20px] text-sm h-screen overflow-hidden gap-[20px] bg-gray-100">
-        <Sidebar />
-        <Content />
+        <Sidebar showSidebar={showSidebar} toggleSidebar={() => toggleSidebar(false)} />
+        <Content
+          toggleSidebar={() => toggleSidebar(true)}
+          showSidebar={showSidebar}
+        />
         <MediaFile />
       </div>
     </>
   );
 }
+
